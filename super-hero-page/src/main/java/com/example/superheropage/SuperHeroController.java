da9c8b0aaab496f21d3126ce8e93f4eefedcdcb3
BDPA02@metrostate.edu <BDPA02@metrostate.edu>
2018-02-24 13:57:24 -0600

    Changes to my Superhero

--- super-hero-page/src/main/java/com/example/superheropage/SuperHeroController.java
+++ super-hero-page/src/main/java/com/example/superheropage/SuperHeroController.java
@@ -8,13 +8,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class SuperHeroController {
     @RequestMapping("/superhero")
     public String greeting(Model model) {
-        model.addAttribute("name", "Super Hero Name");
-        model.addAttribute("power1", "A power that is super");
-        model.addAttribute("power2", "Another power that is super");
-        model.addAttribute("power3", "A third power that is super");
-        model.addAttribute("fact1", "This is fun fact is maybe not so fun");
-        model.addAttribute("fact2", "This is another fun fact");
-        model.addAttribute("fact3", "This is the third ");
+        model.addAttribute("name", " Water Man");
+        model.addAttribute("power1", "Drown his enemies");
+        model.addAttribute("power2", "Solve the clean water crisis");
+        model.addAttribute("power3", "Make himself into different shapes");
+        model.addAttribute("fact1", "Ability to sustain life");
+        model.addAttribute("fact2", "Contains hydrogen molecules");
+        model.addAttribute("fact3", "  ");
 
         return "hero"; //Return "hero" to tell spring to use the hero.html template
     }
